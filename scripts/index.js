da8bcb78621abea90c8cc5b213165ea70fde0565
Emiliano Aceves Hernández <0223332@up.edu.mx>
2021-12-03 13:22:00 -0600

    Versión funcional de la app
    Incluye todas las páginas para el funcionamiento de la misma
    Scripts para crudy todo para auth
    Más imágenes para banners, perfiles, etc

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAakbxO03hY6cWcDzUuEnqH2o0ffYdtBho",
+  authDomain: "aw-gea.firebaseapp.com",
+  projectId: "aw-gea",
+  storageBucket: "aw-gea.appspot.com",
+  messagingSenderId: "397942351709",
+  appId: "1:397942351709:web:eb9d21d8c6bcf3f739eab4",
+  measurementId: "G-9SB5R3K8F3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
